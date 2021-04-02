4882faefad088a665e9b1a057752335720daa8bd
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-02 11:04:57 -0600

    set up for test

---
+++ js/sandbox.js
@@ -0,0 +1,50 @@
+Test 1
+Numbers that contain 1 all digits are replaced with "Beep!"
+
+Test 2
+Numbers that contain 2 all digits are replaced with "BOOP!"
+
+Test 3
+NUmbers that contain a 3 all digits are repalced with "Won't you be my neighbor"
+
+
+Describe: beepBoop()
+Test:"It should return an array with a 0 if the number 0 is inputted"
+Code: beepBoop(0);
+Expected Output:0
+
+
+Describe:turn the string into an array
+Test:
+Code:
+Expected Output:[1, 2, 3]
+
+
+Describe: Isolate the number 1 and convert to the word "Beep!"
+Test:
+Code:
+Expected Output:
+
+
+Describe: Isolate the number 2 and convert to the word "Boop!"
+Test:
+Code:
+Expected Output:
+
+
+Describe: Isolate the number 3 and convert to the string "Won't you be my neighbor?"
+Test:
+Code:
+Expected Output:
+
+
+Describe: return all elements in the string with converted elements 1,2,3 in order
+Test:
+Code:
+Expected Output:
+
+
+Describe: return array back to user on DOM
+Test:
+Code:
+Expected Output:
---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Business Logic
+
+
+
+function convertToArray(input) {
+
+
+}
