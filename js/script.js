a2ecfda0ac227adac72bf6823c528bfbcfe8ffed
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-02 15:35:04 -0600

    set the input to consol.log

--- js/script.js
+++ js/script.js
@@ -2,18 +2,29 @@
 
 
 
-function numbersInput(numbers) {
-  let numberArray = []
-  numbersInput.forEach(function (element) {
+// function numbersInput(numbers) {
+//   let numberArray = []
+//   numbersInput.forEach(function (element) {
     
-  })
-  return "greater than zero";
-}
+//   })
+//   return "greater than zero";
+// }
 
-function number1change(number1) {
-if 
+// function number1change(number1) {
+// if 
 
-}
+// }
+
+// let numbers
 
 
 //User Logic
+
+$(document).ready(function () {
+  $("form#user").submit(function (event) {
+    event.preventDefault();
+    const userInput = $("#userInput").val();
+    
+    console.log(userInput);
+  });
+});
