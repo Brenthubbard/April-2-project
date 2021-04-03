25a3cdb7e7a2a6b7f4f839028dc1f1a3bdce3389
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-02 18:26:16 -0600

    print to consol but not to DOM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 //Business Logic
 
-
+function loopingTheNumber(number) {
+  for (let i = 0; i <= number; i++) {
+    if (i.toString().indexOf("3") > -1) {
+      console.log("Won't you be my neighbor?");
+    } else if (i.toString().indexOf("2") > -1) {
+      console.log("Boop!")
+    } else if (i.toString().indexOf("1") > -1) {
+      console.log("Beep!")
+    } else {
+      console.log(i)
+    }
+  }
+}
 
 // function numbersInput(numbers) {
 //   let numberArray = []
@@ -19,16 +31,6 @@ $(document).ready(function () {
     event.preventDefault();
     const userInput = parseInt($("#userInput").val());
 
-    for (let index = 0; index <= userInput; index++) {
-      if (index.toString().indexOf("3") > -1) {
-        console.log("Won't you be my neighbor?");
-      } else if (index.toString().indexOf("2") > -1) {
-        console.log("Boop!")
-      } else if (index.toString().indexOf("1") > -1) {
-        console.log("Beep!")
-      } else {
-        console.log(index)
-      }
-    }
+    loopingTheNumber
   });
 });
